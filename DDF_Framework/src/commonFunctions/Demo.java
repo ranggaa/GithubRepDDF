f2547af4f7fa58cd572820f87632ef480702e290
MAXHUB <MAXHUB@DESKTOP-T6KLQOF>
2023-10-06 10:30:47 +0530

    New Class Created with methods

---
+++ DDF_Framework/src/commonFunctions/Demo.java
@@ -0,0 +1,22 @@
+package commonFunctions;
+
+public class Demo {
+public void add()
+{
+	int a=34,b=87,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=466,b=6,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void mul()
+{
+	int a=678,b=5,c;
+	c=a+b;
+	System.out.println(c);
+}
+}
