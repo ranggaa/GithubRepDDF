046ccaf35253d4a48eac72a2f46a14feb22acfcd
MAXHUB <MAXHUB@DESKTOP-T6KLQOF>
2023-10-06 10:32:48 +0530

    Modified Values in method

--- DDF_Framework/src/commonFunctions/Demo.java
+++ DDF_Framework/src/commonFunctions/Demo.java
@@ -3,19 +3,19 @@ package commonFunctions;
 public class Demo {
 public void add()
 {
-	int a=34,b=87,c;
+	int a=57567,b=989,c;
 	c=a+b;
 	System.out.println(c);
 }
 public void div()
 {
-	int a=466,b=6,c;
+	int a=53454,b=5654,c;
 	c=a+b;
 	System.out.println(c);
 }
 public void mul()
 {
-	int a=678,b=5,c;
+	int a=6546,b=6655,c;
 	c=a+b;
 	System.out.println(c);
 }
